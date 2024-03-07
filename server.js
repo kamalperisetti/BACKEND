461fdad6588c27b00f277671bdf1bbba9bf372f0
kamalperisetti <perisettikamal@gmail.com>
2024-03-07 11:42:21 +0530

    port changed

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require("cors")
 
 
 const app = express()
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
+const port = 5000
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
